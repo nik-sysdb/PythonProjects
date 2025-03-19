dfa6692b2383ad3fe4d5524285c33191254367c5
Niraj Kumar <nkniraj59@gmail.com>
2025-03-19 15:01:35 +0530

    Add files via upload

---
+++ QuizGame/data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Python is a case sensitive language.", "answer": "True"},
+    {"text": "Python is object-oriented language.", "answer": "True"},
+    {"text": "Python is a scripting language.", "answer": "True"},
+    {"text": "Python runs on the chrome browser.", "answer": "False"},
+    {"text": "Python is a dynamically typed language.", "answer": "True"},
+    {"text": "Python is a machine language.", "answer": "False"},
+    {"text": "Python is a open source language.", "answer": "True"},
+    {"text": "Python is a compiled language.", "answer": "False"},
+    {"text": "Python is developed by Guido van Rossum.", "answer": "True"},
+    {"text": "Python is written in c language.", "answer": "True"},
+    {"text": "Python django library is used for game development.", "answer": "False"},
+    {"text": " Python is a hard to learn language.", "answer": "False"}
+]
